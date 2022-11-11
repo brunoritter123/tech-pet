91bb2fb389d9b98dab1910b3e26963cd4639ea15
brunoritter123 <brunosk8123@hotmail.com>
2022-11-10 21:39:16 -0300

    ajustes empresa

---
+++ tech-pet-bff/TechPet.Identity/Data/Maps/EmpresaMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechPet.Identity.Entities;
+
+namespace TechPet.Identity.Data.Maps
+{
+    public class EmpresaMap : IEntityTypeConfiguration<Empresa>
+    {
+        public void Configure(EntityTypeBuilder<Empresa> builder)
+        {
+            builder.ToTable(nameof(Empresa));
+
+            builder.HasKey(x => x.Codigo);
+
+            //builder.HasMany(cr => cr.Users)
+            //    .WithOne(x => x.CodigoEmpresa);
+        }
+    }
+}
---
+++ tech-pet-bff/TechPet.Identity/Entities/Empresa.cs
@@ -0,0 +1,21 @@
+﻿namespace TechPet.Identity.Entities
+{
+    public class Empresa
+    {
+        public string Codigo { get; }
+        public string Nome { get; }
+        public string NomeFantasia { get; }
+        public string Cnpj { get; }
+
+        public IEnumerable<User> Users { get; set; }
+
+        public Empresa(string codigo, string nome, string nomeFantasia, string cnpj, IEnumerable<User> users)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            NomeFantasia = nomeFantasia;
+            Cnpj = cnpj;
+            Users = users;
+        }
+    }
+}
